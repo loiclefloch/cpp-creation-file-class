6bb6514ff5e4e69a060772da9e878280bd1555da
loic lefloch <l.lefloch.91@gmail.com>
2014-11-04 18:12:48 +0100

    add configuration file for username

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 dir=~/.cpp_creation_file_class/
+config_filename=config.cfg
 
 echo "create directory $dir"
 mkdir -p $dir
 echo "copy configuration files"
 cp -r ./template_files/ $dir
 
+echo "create configuration file $dir/$config_filename"
+touch "$dir/$config_filename"
+echo "username=\"$USER\"" > "$dir/$config_filename";
+
 echo "done. Enjoy your life !"
+
